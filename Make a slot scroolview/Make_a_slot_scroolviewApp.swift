9fd5e4083ac26dc5a57da414d51a04bcc9259509
Steven Hertz <hertzsteven@icloud.com>
2024-06-04 07:19:43 -0400

    Initial Commit

---
+++ Make a slot scroolview/Make_a_slot_scroolviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_a_slot_scroolviewApp.swift
+//  Make a slot scroolview
+//
+//  Created by Steven Hertz on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Make_a_slot_scroolviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
